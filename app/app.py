52fe898575eae02ea6da3329c1e1cf1127ed266e
richebyte <richardgrayson2161@gmail.com>
2025-09-22 16:39:26 +0530

    app started to create

--- app/app.py
+++ app/app.py
@@ -2,3 +2,13 @@ from fastapi import FastAPI
 from pydantic import baseModel
 import torch
 import os 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+MODEL_PATH = "./model"
+
+app = FastAPI(
+    title = "Suicidal Text Classifier API"
+    description="Detects suicidal ideation in text using a fine-tuned BERT model.",
+    version="1.0.0"
+)
